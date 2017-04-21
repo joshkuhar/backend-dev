18e56c7f410628ea234f92967c0b074f5d912927
Josh Kuhar <jdkuhar@gmail.com>
2017-04-21 19:15:59 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+// app.use(bodyParser.json(urlncoded({extended: false}));
+
+var username = "foo";
+var password = "bar";
+
+app.post('/login', function(req, res) {
+  var checkUsername = req.body.username == username ? username : 'Wrong username';
+  var checkPassword = req.body.password == password ? password : 'Wrong password';
+  res.json({
+    username: checkUsername,
+    password: checkPassword
+  });
+});
+
+app.use('*', function(req, res) {
+  res.status(404).json({
+    message: 'Not Found'
+  })
+});
+
+app.listen(process.env.PORT || 8080, process.env.IP);
