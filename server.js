01c615ec6c57ad7478e4672fc6424665313124a7
Josh Kuhar <jdkuhar@gmail.com>
2017-07-13 15:37:43 -0400

    added runs

--- server.js
+++ server.js
@@ -8,11 +8,32 @@ app.use(bodyParser.json());
 
 var runs = [
   {
-    date: 'June 29',
-    name: `Morgan's Pier`,
-    address: '221 N Christopher Columbus Blvd',
-    lat: 39.955033,
-    lng: -75.138913
+    date: 'July 13, 2017',
+    name: 'The Deck',
+    address: '401 S Columbus Blvd',
+    lat: 39.941598,
+    lng: -75.141249
+  },
+  {
+    date: 'July 20, 2017',
+    name: 'Veterans Boxing Association',
+    address: '2733 E Clearfield St',
+    lat: 39.982194,
+    lng: -75.104085
+  },
+  {
+    date: 'July 27, 2017',
+    name: 'Evil Genius',
+    address: '1727 N Front Street',
+    lat: 39.975714,
+    lng: -75.134163
+  },
+  {
+    date: 'August 3, 2017',
+    name: 'The Oval',
+    address: '2451 Benjamin Franklin Pkwy',
+    lat: 39.963285,
+    lng: -75.177137
   }
 ]
 
@@ -21,7 +42,7 @@ app.get('/', function(req,res){
 });
 
 app.get('/runs', function(req, res){
-  res.status(200).json(runs[0])
+  res.status(200).json(runs)
 })
 
 
@@ -32,3 +53,11 @@ app.use('*', function(req, res) {
 });
 
 app.listen(process.env.PORT || 8080, process.env.IP);
+
+  // {
+  //   date: 'July 13, 2017',
+  //   name: 'The Moshulu',
+  //   address: '401 S Columbus Blvd',
+  //   lat: 39.941598,
+  //   lng: -75.141249
+  // }pin
